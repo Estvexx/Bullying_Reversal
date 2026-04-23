e64d7adbffc80666611546a1594c4a121dc60a66
Estvexx <franciscoesteves2016@gmail.com>
2026-04-23 22:07:55 +0100

    feat: particulas de chao e apanhar moedas

--- Assets/Scripts/Books/BookPickup.cs
+++ Assets/Scripts/Books/BookPickup.cs
@@ -1,20 +1,25 @@
-    using UnityEngine;
+using UnityEngine;
 
-    public class BookPickup : MonoBehaviour
+public class BookPickup : MonoBehaviour
+{
+    private BookManager bookManager;
+    public GameObject efeitoMoeda;
+
+    void Start()
     {
-        private BookManager bookManager;
+        bookManager = FindFirstObjectByType<BookManager>();
+    }
 
-        void Start()
-        {
-            bookManager = FindFirstObjectByType<BookManager>();
-        }
-
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Book"))
         {
             bookManager.AdicionarLivro();
+
+            GameObject efeito = Instantiate(efeitoMoeda, other.transform.position, Quaternion.identity);
+            Destroy(efeito, 1f);
+
             other.gameObject.SetActive(false);
         }
     }
-    }
+}
